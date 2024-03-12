4c563a639c8f04a07502dafc3316e6fde5af14ab
LeviAsiala <74027693+LeviAsiala@users.noreply.github.com>
2024-03-12 00:34:18 -0400

    new user pool stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,8 +2,18 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "futurerec2": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "futurerec205b837c475b837c47": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   }
 }
